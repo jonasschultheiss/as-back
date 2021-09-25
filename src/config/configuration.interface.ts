ea6a24941bfc7e867aaed6e799c6e806686837dd
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2021-09-26 00:31:49 +0200

    :sparkles: added configModule

---
+++ src/config/configuration.interface.ts
@@ -0,0 +1,4 @@
+export default interface IConfiguration {
+  port: number;
+  databaseUrl: string;
+}
---
+++ src/config/configuration.ts
@@ -0,0 +1,6 @@
+import IConfiguration from './configuration.interface';
+
+export default (): IConfiguration => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  databaseUrl: process.env.DATABASE_URL,
+});
