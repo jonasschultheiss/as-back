adc2129d5d8041ab1478eecf171ef3de843db5dd
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2021-09-26 00:55:33 +0200

    :sparkles: added TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] })],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('databaseUrl'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        synchronize: false,
+        migrationsRun: configService.get('NODE_ENV') === 'production',
+        logging: true,
+        logger: 'file',
+        retryAttempts: 10,
+        ssl: configService.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : undefined,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
