dff35b2d002653bf1c016dbea984f9172479c55c
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2021-09-26 00:42:47 +0200

    :wrench: added basic nest/express configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  await app.listen(port);
 }
 bootstrap();
